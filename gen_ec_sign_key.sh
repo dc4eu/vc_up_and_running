8da1e74a72b46beb791bacf22dd30333ead5cf94
masv3971 <masv@sunet.se>
2024-10-17 10:51:34 +0200

    Prevent script from running more than once.

--- gen_ec_sign_key.sh
+++ gen_ec_sign_key.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -f private_ec256.pem ]; then
+    exit 0
+fi
+
 # generate a private key for a curve
 openssl ecparam -name prime256v1 -genkey -noout -out private_ec256.pem
 
