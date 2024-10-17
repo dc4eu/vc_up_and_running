072d435d12ebdbe767abd8be0784654880a4aa24
masv3971 <masv@sunet.se>
2024-10-17 11:05:34 +0200

    Include key script in start script.

--- start.sh
+++ start.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
-printf "Starting vc docker-compose services\n"
+
 ./gen_ec_sign_key.sh
+if [ ! -f private_ec256.pem ]; then
+    printf "Create signing key\n"
+    # generate a private key for a curve
+    openssl ecparam -name prime256v1 -genkey -noout -out private_ec256.pem
+
+    # generate corresponding public key
+    openssl ec -in private_ec256.pem -pubout -out public_ec256.pem
+fi
+
+printf "Starting vc docker-compose services\n"
 docker compose -f docker-compose.yaml up -d --remove-orphans
