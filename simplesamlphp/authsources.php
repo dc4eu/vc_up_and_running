5679f6def067cac393cf5520e3196cba291dc5f3
Mikael Frykholm <mifr@sunet.se>
2024-11-18 17:50:26 +0100

    Add example users to userdb

--- simplesamlphp/authsources.php
+++ simplesamlphp/authsources.php
@@ -2,31 +2,30 @@
 $config = [
     'example-userpass' => [
         'exampleauth:UserPass',
-
         'student:password' => [
             'uid' => ['stud123'],
-                'eduPersonPrincipalName' => ['stud123@qa.swamid.se'],
-                'mail' => 'stud123@example.swamid.se',
-                'givenName' => 'Stina',
-                'sn' => 'Student',
-                'cn' => 'Stina Student',
-                'displayName' => 'Stina Student',
-                'schacHomeOrganization' => 'knappologiska.se',
-                'eduPersonAffiliation' => ['member', 'student'],
-                'eduPersonScopedAffiliation' => ['member@qa.swamid.se', 'student@qa.swamid.se'],
-                'eduPersonAssurance' => [
+            'eduPersonPrincipalName' => ['stud123@qa.swamid.se'],
+            'mail' => 'stud123@example.swamid.se',
+            'givenName' => 'Stina',
+            'sn' => 'Student',
+            'cn' => 'Stina Student',
+            'displayName' => 'Stina Student',
+            'schacHomeOrganization' => 'knappologiska.se',
+            'eduPersonAffiliation' => ['member', 'student'],
+            'eduPersonScopedAffiliation' => ['member@qa.swamid.se', 'student@qa.swamid.se'],
+            'eduPersonAssurance' => [
                 'http://www.swamid.se/policy/assurance/al1',
                 'https://refeds.org/assurance',
                 'https://refeds.org/assurance/ID/unique',
                 'https://refeds.org/assurance/ID/eppn-unique-no-reassign',
                 'https://refeds.org/assurance/IAP/low',
-                        'https://refeds.org/assurance/ATP/ePA-1m',
-                ],
+                'https://refeds.org/assurance/ATP/ePA-1m',
+            ],
             'eduPersonOrcid' => 'https://orcid.org/0000-0002-1825-0097',
             'norEduPersonNIN' => '197912162398',
             'personalIdentityNumber' => '197912162398',
             'schacDateOfBirth' => '12345678',
-            'mailLocalAddress' => ['stud123@qa.swamid.se','stud124@qa.swamid.se'],
+            'mailLocalAddress' => ['stud123@qa.swamid.se', 'stud124@qa.swamid.se'],
             'o' => 'Swedish Academic Identity Federation',
             'norEduOrgAcronym' => 'SWAMID',
             'c' => 'SE',
@@ -35,9 +34,409 @@ $config = [
         ],
         'Carlos:1234' => [
             'subject-id' => '10',
-	    'schacDateOfBirth' => '1970-01-10',
+            'schacDateOfBirth' => '1970-01-10',
             'givenName' => 'Carlos',
             'surname' => 'Castaneda',
+        ],
+        'deniro:deniro' => [
+            'uid' => ['deniro'],
+            'givenName' => 'Robert',
+            'surname' => 'De Niro'
+        ],
+        'dench:dench' => [
+            'uid' => ['dench'],
+            'givenName' => 'Judi',
+            'surname' => 'Dench'
+        ],
+        'depp:depp' => [
+            'uid' => ['depp'],
+            'givenName' => 'Johnny',
+            'surname' => 'Depp'
+        ],
+        'dicaprio:dicaprio' => [
+            'uid' => ['dicaprio'],
+            'givenName' => 'Leonardo',
+            'surname' => 'DiCaprio'
+        ],
+        'downey:downey' => [
+            'uid' => ['downey'],
+            'givenName' => 'Robert',
+            'surname' => 'Downey Jr.'
+        ],
+        'driver:driver' => [
+            'uid' => ['driver'],
+            'givenName' => 'Adam',
+            'surname' => 'Driver'
+        ],
+        'elba:elba' => [
+            'uid' => ['elba'],
+            'givenName' => 'Idris',
+            'surname' => 'Elba'
+        ],
+        'farrell:farrell' => [
+            'uid' => ['farrell'],
+            'givenName' => 'Colin',
+            'surname' => 'Farrell'
+        ],
+        'fassbender:fassbender' => [
+            'uid' => ['fassbender'],
+            'givenName' => 'Michael',
+            'surname' => 'Fassbender'
+        ],
+        'foster:foster' => [
+            'uid' => ['foster'],
+            'givenName' => 'Jodie',
+            'surname' => 'Foster'
+        ],
+        'foxx:foxx' => [
+            'uid' => ['foxx'],
+            'givenName' => 'Jamie',
+            'surname' => 'Foxx'
+        ],
+        'freeman:freeman' => [
+            'uid' => ['freeman'],
+            'givenName' => 'Morgan',
+            'surname' => 'Freeman'
+        ],
+        'glover:glover' => [
+            'uid' => ['glover'],
+            'givenName' => 'Donald',
+            'surname' => 'Glover'
+        ],
+        'hanks:hanks' => [
+            'uid' => ['hanks'],
+            'givenName' => 'Tom',
+            'surname' => 'Hanks'
+        ],
+        'saldana:saldana' => [
+            'uid' => ['saldana'],
+            'givenName' => 'Zoe',
+            'surname' => 'Saldana'
+        ],
+        'hathaway:hathaway' => [
+            'uid' => ['hathaway'],
+            'givenName' => 'Anne',
+            'surname' => 'Hathaway'
+        ],
+        'hayek:hayek' => [
+            'uid' => ['hayek'],
+            'givenName' => 'Salma',
+            'surname' => 'Hayek'
+        ],
+        'hemsworth:hemsworth' => [
+            'uid' => ['hemsworth'],
+            'givenName' => 'Chris',
+            'surname' => 'Hemsworth'
+        ],
+        'hopkins:hopkins' => [
+            'uid' => ['hopkins'],
+            'givenName' => 'Anthony',
+            'surname' => 'Hopkins'
+        ],
+        'jackman:jackman' => [
+            'uid' => ['jackman'],
+            'givenName' => 'Hugh',
+            'surname' => 'Jackman'
+        ],
+        'jackson:jackson' => [
+            'uid' => ['jackson'],
+            'givenName' => 'Samuel L.',
+            'surname' => 'Jackson'
+        ],
+        'johansson:johansson' => [
+            'uid' => ['johansson'],
+            'givenName' => 'Scarlett',
+            'surname' => 'Johansson'
+        ],
+        'johnson:johnson' => [
+            'uid' => ['johnson'],
+            'givenName' => 'Dakota',
+            'surname' => 'Johnson'
+        ],
+        'kidman:kidman' => [
+            'uid' => ['kidman'],
+            'givenName' => 'Nicole',
+            'surname' => 'Kidman'
+        ],
+        'kirby:kirby' => [
+            'uid' => ['kirby'],
+            'givenName' => 'Vanessa',
+            'surname' => 'Kirby'
+        ],
+        'kunis:kunis' => [
+            'uid' => ['kunis'],
+            'givenName' => 'Mila',
+            'surname' => 'Kunis'
+        ],
+        'lawrence:lawrence' => [
+            'uid' => ['lawrence'],
+            'givenName' => 'Jennifer',
+            'surname' => 'Lawrence'
+        ],
+        'mcadams:mcadams' => [
+            'uid' => ['mcadams'],
+            'givenName' => 'Rachel',
+            'surname' => 'McAdams'
+        ],
+        'mcconaughey:mcconaughey' => [
+            'uid' => ['mcconaughey'],
+            'givenName' => 'Matthew',
+            'surname' => 'McConaughey'
+        ],
+        'mcgregor:mcgregor' => [
+            'uid' => ['mcgregor'],
+            'givenName' => 'Ewan',
+            'surname' => 'McGregor'
+        ],
+        'mirren:mirren' => [
+            'uid' => ['mirren'],
+            'givenName' => 'Helen',
+            'surname' => 'Mirren'
+        ],
+        'momoa:momoa' => [
+            'uid' => ['momoa'],
+            'givenName' => 'Jason',
+            'surname' => 'Momoa'
+        ],
+        'oldman:oldman' => [
+            'uid' => ['oldman'],
+            'givenName' => 'Gary',
+            'surname' => 'Oldman'
+        ],
+        'pacino:pacino' => [
+            'uid' => ['pacino'],
+            'givenName' => 'Al',
+            'surname' => 'Pacino'
+        ],
+        'pfeiffer:pfeiffer' => [
+            'uid' => ['pfeiffer'],
+            'givenName' => 'Michelle',
+            'surname' => 'Pfeiffer'
+        ],
+        'phoenix:phoenix' => [
+            'uid' => ['phoenix'],
+            'givenName' => 'Joaquin',
+            'surname' => 'Phoenix'
+        ],
+        'pitt:pitt' => [
+            'uid' => ['pitt'],
+            'givenName' => 'Brad',
+            'surname' => 'Pitt'
+        ],
+        'portman:portman' => [
+            'uid' => ['portman'],
+            'givenName' => 'Natalie',
+            'surname' => 'Portman'
+        ],
+        'pratt:pratt' => [
+            'uid' => ['pratt'],
+            'givenName' => 'Chris',
+            'surname' => 'Pratt'
+        ],
+        'reeves:reeves' => [
+            'uid' => ['reeves'],
+            'givenName' => 'Keanu',
+            'surname' => 'Reeves'
+        ],
+        'reilly:reilly' => [
+            'uid' => ['reilly'],
+            'givenName' => 'John C.',
+            'surname' => 'Reilly'
+        ],
+        'reynolds:reynolds' => [
+            'uid' => ['reynolds'],
+            'givenName' => 'Ryan',
+            'surname' => 'Reynolds'
+        ],
+        'robbie:robbie' => [
+            'uid' => ['robbie'],
+            'givenName' => 'Margot',
+            'surname' => 'Robbie'
+        ],
+        'roberts:roberts' => [
+            'uid' => ['roberts'],
+            'givenName' => 'Julia',
+            'surname' => 'Roberts'
+        ],
+        'rudd:rudd' => [
+            'uid' => ['rudd'],
+            'givenName' => 'Paul',
+            'surname' => 'Rudd'
+        ],
+        'smith:smith' => [
+            'uid' => ['smith'],
+            'givenName' => 'Will',
+            'surname' => 'Smith'
+        ],
+        'spencer:spencer' => [
+            'uid' => ['spencer'],
+            'givenName' => 'Octavia',
+            'surname' => 'Spencer'
+        ],
+        'stewart:stewart' => [
+            'uid' => ['stewart'],
+            'givenName' => 'Kristen',
+            'surname' => 'Stewart'
+        ],
+        'stone:stone' => [
+            'uid' => ['stone'],
+            'givenName' => 'Emma',
+            'surname' => 'Stone'
+        ],
+        'streep:streep' => [
+            'uid' => ['streep'],
+            'givenName' => 'Meryl',
+            'surname' => 'Streep'
+        ],
+        'swinton:swinton' => [
+            'uid' => ['swinton'],
+            'givenName' => 'Tilda',
+            'surname' => 'Swinton'
+        ],
+        'tatum:tatum' => [
+            'uid' => ['tatum'],
+            'givenName' => 'Channing',
+            'surname' => 'Tatum'
+        ],
+        'theron:theron' => [
+            'uid' => ['theron'],
+            'givenName' => 'Charlize',
+            'surname' => 'Theron'
+        ],
+        'vaughn:vaughn' => [
+            'uid' => ['vaughn'],
+            'givenName' => 'Vince',
+            'surname' => 'Vaughn'
+        ],
+        'vikander:vikander' => [
+            'uid' => ['vikander'],
+            'givenName' => 'Alicia',
+            'surname' => 'Vikander'
+        ],
+        'waltz:waltz' => [
+            'uid' => ['waltz'],
+            'givenName' => 'Christoph',
+            'surname' => 'Waltz'
+        ],
+        'washington:washington' => [
+            'uid' => ['washington'],
+            'givenName' => 'Denzel',
+            'surname' => 'Washington'
+        ],
+        'watts:watts' => [
+            'uid' => ['watts'],
+            'givenName' => 'Naomi',
+            'surname' => 'Watts'
+        ],
+        'williams:williams' => [
+            'uid' => ['williams'],
+            'givenName' => 'Michelle',
+            'surname' => 'Williams'
+        ],
+        'wilson:wilson' => [
+            'uid' => ['wilson'],
+            'givenName' => 'Owen',
+            'surname' => 'Wilson'
+        ],
+        'winslet:winslet' => [
+            'uid' => ['winslet'],
+            'givenName' => 'Kate',
+            'surname' => 'Winslet'
+        ],
+        'witherspoon:witherspoon' => [
+            'uid' => ['witherspoon'],
+            'givenName' => 'Reese',
+            'surname' => 'Witherspoon'
+        ],
+        'adams:adams' => [
+            'uid' => ['adams'],
+            'givenName' => 'Amy',
+            'surname' => 'Adams'
+        ],
+        'affleck:affleck' => [
+            'uid' => ['affleck'],
+            'givenName' => 'Ben',
+            'surname' => 'Affleck'
+        ],
+        'bancroft:bancroft' => [
+            'uid' => ['bancroft'],
+            'givenName' => 'Anne',
+            'surname' => 'Bancroft'
+        ],
+        'berry:berry' => [
+            'uid' => ['berry'],
+            'givenName' => 'Halle',
+            'surname' => 'Berry'
+        ],
+        'biel:biel' => [
+            'uid' => ['biel'],
+            'givenName' => 'Jessica',
+            'surname' => 'Biel'
+        ],
+        'blanchett:blanchett' => [
+            'uid' => ['blanchett'],
+            'givenName' => 'Cate',
+            'surname' => 'Blanchett'
+        ],
+        'blunt:blunt' => [
+            'uid' => ['blunt'],
+            'givenName' => 'Emily',
+            'surname' => 'Blunt'
+        ],
+        'boyega:boyega' => [
+            'uid' => ['boyega'],
+            'givenName' => 'John',
+            'surname' => 'Boyega'
+        ],
+        'bullock:bullock' => [
+            'uid' => ['bullock'],
+            'givenName' => 'Sandra',
+            'surname' => 'Bullock'
+        ],
+        'cage:cage' => [
+            'uid' => ['cage'],
+            'givenName' => 'Nicolas',
+            'surname' => 'Cage'
+        ],
+        'carell:carell' => [
+            'uid' => ['carell'],
+            'givenName' => 'Steve',
+            'surname' => 'Carell'
+        ],
+        'chastain:chastain' => [
+            'uid' => ['chastain'],
+            'givenName' => 'Jessica',
+            'surname' => 'Chastain'
+        ],
+        'close:close' => [
+            'uid' => ['close'],
+            'givenName' => 'Glenn',
+            'surname' => 'Close'
+        ],
+        'craig:craig' => [
+            'uid' => ['craig'],
+            'givenName' => 'Daniel',
+            'surname' => 'Craig'
+        ],
+        'cruz:cruz' => [
+            'uid' => ['cruz'],
+            'givenName' => 'Penélope',
+            'surname' => 'Cruz'
+        ],
+        'cumberbatch:cumberbatch' => [
+            'uid' => ['cumberbatch'],
+            'givenName' => 'Benedict',
+            'surname' => 'Cumberbatch'
+        ],
+        'damon:damon' => [
+            'uid' => ['damon'],
+            'givenName' => 'Matt',
+            'surname' => 'Damon'
+        ],
+        'davis:davis' => [
+            'uid' => ['davis'],
+            'givenName' => 'Viola',
+            'surname' => 'Davis'
         ],
     ],
 ];
